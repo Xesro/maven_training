5df692e6b13dbc72bc6cd2f895328323825e715c
Arthur Plebs <arthur.plebs@outlook.fr>
2022-03-02 17:38:55 +0100

    Add live badges

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # maven_training
+
+![build CI](https://github.com/Xesro/maven_training/actions/workflows/build.yml/badge.svg)
+[![codecov](https://codecov.io/gh/Xesro/maven_training/branch/main/graph/badge.svg?token=2aKhng8GxY)](https://codecov.io/gh/Xesro/maven_training)
--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class SampleTest {
 
@@ -22,5 +24,13 @@ class SampleTest {
     void fact() {
         int result = sample.fact(4);
         Assertions.assertThat(result).as("fact de 4").isEqualTo(24);
+    }
+
+    @Test
+    void failFact() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+            sample.fact(-1));
+        Assertions.assertThat(exception.getMessage()).isEqualTo("N should be positive");
+
     }
 }
